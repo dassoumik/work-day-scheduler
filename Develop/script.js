59c11260e6b8fed3337c8ffee9a6475653dbcd79
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-01-20 20:34:36 -0500

    add date change wise refresh text logic

--- Develop/script.js
+++ Develop/script.js
@@ -12,13 +12,15 @@ timeBlock.text(hourMer);
 $("#currentDay").text(currDate);
 
 
-var notesInputs = {"12AM": "", "1AM": "", "2AM": "", "3AM": "", "4AM": "", "5AM": "",
+var notesInputs = {"date": "", "12AM": "", "1AM": "", "2AM": "", "3AM": "", "4AM": "", "5AM": "",
                    "6AM": "", "7AM": "", "8AM": "", "9AM": "", "10AM": "", "11AM": "", 
                    "12PM": "", "1PM": "", "2PM": "", "3PM": "", "4PM": "", "5PM": "",
                    "6PM": "", "7PM": "", "8PM": "", "9PM": "", "10PM": "", "11PM": ""};
 var notesTemp = JSON.parse(localStorage.getItem("notesInputsLS"));
-if (notesTemp != null) {
+if ((notesTemp != null) && (notesTemp.date === currDate)) {
     notesInputs = notesTemp;
+} else {
+    notesInputs.date = currDate;
 } 
 
 var i = 5;
@@ -50,8 +52,11 @@ $(textValue[j]).val(notesInputs[textBlkData]);
         $(textValue[j]).val(notesInputs[textBlkData]);    
     }
    var  textBlkData = $(timeBlock[4]).text();   
-   $(textValue[4]).val(notesInputs[textBlkData]);    
-
+   $(textValue[4]).val(notesInputs[textBlkData]);  
+   timeBlock = $(".time-block");  
+   $(".time-block").first().attr("style", "border-top: solid; border-top-width: 2px; border-top-color: lightgrey;");
+   $(".time-block").last().attr("style", "border-bottom-width: 2px;");
+   console.log(timeBlock.first().text());
   }  
 } while (i > 1);
 
